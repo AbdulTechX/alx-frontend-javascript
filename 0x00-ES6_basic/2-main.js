b1d2971ea034dec59f59af3eef1cf75de5d83f2c
AbdulTechX <abdulazeezbuhari27@gmail.com>
2024-07-26 18:15:59 -0700

    arrow

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from './2-arrow.js';
+
+const neigborhoodsList = new getNeighborhoodsList();
+const res = neigborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
